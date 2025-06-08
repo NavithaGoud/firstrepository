80cbe342229410ecca9bc16653d942db652b8c93
NavithaGoud <peddinavithagoud@gmail.com>
2025-06-08 19:09:03 +0530

    added para

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
      <h1> Creating First Remote Repository in git-hub </h1>
+     <p>GitHub's collaborative approach to development depends on publishing commits from your local repository to GitHub for other people to view, fetch, and update</p>
     </div>
   );
 }
